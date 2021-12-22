589c45d842229948ce23dab14ca5efd5d410ac80
Daniil Petrishin <daynonixx@gmail.com>
2021-12-22 16:48:56 +0200

    Extending cart

--- theme/assets/cart.js
+++ theme/assets/cart.js
@@ -7,12 +7,17 @@ class CustomCart extends HTMLElement {
     this.icon = this.querySelector('.cart-icon');
 
     this.icon.addEventListener('click', ()=>this.open());
-    this.popup.addEventListener('click', (e)=>this.close(e))
+    this.popup.addEventListener('click', (e)=>this.close(e));
+    document.addEventListener('DOMContentLoaded', ()=> this.getCart());
 
+    // Observer to catch new entries in cart
+
+    const cartObserver = () =>{
+
+    }
   }
   open(){
       this.popup.classList.add('open');
-      this.getCart();
   }
   close(e){
     if (e.target === this.popup){
@@ -20,22 +25,67 @@ class CustomCart extends HTMLElement {
     }
   }
   getCart(){
-    fetch('/cart.js')
+    const options = {
+      method: 'GET',
+      dataType: 'json',
+    }
+    fetch('/cart.js', options)
       .then((response) => response.json())
       .then(data => {
-        console.log(data.items)
-        data.items.forEach(item => this.addItem(item));
+        data.items.forEach((item, index) => this.addItem(item, index));
+        this.icon.dataset.count = data.item_count;
+        this.querySelector('.cart-popup_head .counter').innerHTML = data.item_count;
+      })
+      .then(()=> {
+        this.quantity = this.querySelectorAll(".quantity input.quantity__input");
+        this.quantity.forEach((input)=>{
+          if (!input.hasAttribute('indexed')) {
+            input.setAttribute('indexed','');
+            input.addEventListener("change", (e) => {
+              this.update(e.target.dataset.index, e.target.value);
+            });
+          }
+        });
       })
   }
-  addItem(data){
+  addItem(data, index){
     const item = this.template.content.cloneNode(true);
-    item.querySelector('.title').innerHTML = data.title;
-    this.body.appendChild(item)
+
+    if (!this.querySelector('#id_'+(data.id).toString())){
+      item.querySelector('.cart-item').id = 'id_'+(data.id).toString();
+      item.querySelector('.title').innerHTML = data.title;
+
+      item.querySelector('input.quantity__input').value = data.quantity;
+      item.querySelector('input.quantity__input').dataset.index = data.id;
+      item.querySelector('input.quantity__input').id += data.id;
+      item.querySelector('input.quantity__input').setAttribute('name', 'quantity_' + index);
+
+      item.querySelector('.cart-item_img').style.background = "url(" + data.image + ") no-repeat top/cover";
+      item.querySelector('input.item_id').setAttribute('name', 'id_' + index);
+
+      this.body.appendChild(item);
+    }
   }
-  update(){};
-  clean(){};
-  appendItem(){
-    this.body.appendChild(this.template.cloneNode(true))
+  deleteItem(index){
+    this.update(index, 0)
   }
+  update(index, quantity){
+    const xhr = new XMLHttpRequest();
+    let body = JSON.stringify({ updates: {[index]: quantity }, section: 'cart-item' });
+    this.body.classList.add('loading');
+
+    xhr.open('POST', '/cart/update.js', true );
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(body);
+    xhr.onload = () =>{
+      this.getCart();
+      this.body.classList.remove('loading');
+    }
+  };
+  clean(){
+    fetch('/cart/clear.js')
+      .then(response => response.json())
+      .then(()=> this.getCart())
+  };
 }
 customElements.define('custom-cart', CustomCart);
